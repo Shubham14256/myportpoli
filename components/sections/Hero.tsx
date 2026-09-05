08ffb38b125028c3680bc9eb52cbadf3b4af8fda
SHUBHAM ANNASAHEB SOLAT <shubhamsolat36@gmail.com>
2026-09-05 10:48:19 +0530

    Update download link in Hero component
    Signed-off-by: SHUBHAM ANNASAHEB SOLAT <shubhamsolat36@gmail.com>

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -101,7 +101,7 @@ export const Hero = () => {
                 View My Work
               </motion.a>
               <motion.a
-                href="https://drive.google.com/file/d/1dyOp7Tj2kqJnj0_MGUEynN7MkgOoTuXE/view?usp=sharing"
+                href="https://drive.google.com/file/d/1CpMjpDUy2J7OH7M-OfAhgOu5N8QJkQLC/view?usp=sharing"
                 download
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.95 }}
